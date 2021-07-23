56029085785bb2ec8234b59858b12f2556cd6947
HristoDzhambazki1 <hristo.dzhambazki@barinlabs.com>
2021-07-23 14:42:53 +0300

    added categories and channels data in modal

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,7 +4,11 @@ import { closeDevice } from "../../redux/CurrentDevice/currentDeviceSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faTimes, faBan } from "@fortawesome/free-solid-svg-icons";
 import styles from "./sideBar.module.scss";
+import { useEffect, useState } from "react";
 
+const _WEATHER = "Weather";
+const _GASES = "Gases";
+const _PARTICULATES = "Particulates";
 const SideBar = () => {
   const dispatch = useDispatch();
   const error = useSelector((state) => state.currentDevice.error);
@@ -12,6 +16,27 @@ const SideBar = () => {
   const device = useSelector((state) => state.currentDevice.device);
   const { data, status } = device;
   const { online, lastSubmissionShort } = status;
+  const [weatherChannels, setWeatherChannels] = useState([]);
+  const [gasesChannels, setGasesChannels] = useState([]);
+  const [particulatesChannels, setParticulatesChannels] = useState([]);
+
+  useEffect(() => {
+    setWeatherChannels([]);
+    setGasesChannels([]);
+    setParticulatesChannels([]);
+    if (!error) {
+      data.forEach((category) => {
+        const channels = category.channels;
+        if (category.name === _WEATHER) {
+          setWeatherChannels(channels);
+        } else if (category.name === _GASES) {
+          setGasesChannels(channels);
+        } else if (category.name === _PARTICULATES) {
+          setParticulatesChannels(channels);
+        }
+      });
+    }
+  }, [data, error]);
 
   const statusCircleColor = error ? "red" : online ? "green" : "red";
   const statusIcon = error ? (
@@ -38,6 +63,59 @@ const SideBar = () => {
       {!error && (
         <div>
           <div>Last Submission: {lastSubmissionShort}</div>
+          <div>
+            {weatherChannels.length > 0 && (
+              <>
+                <h3>Weather:</h3>
+                <div>
+                  {weatherChannels.map((channel) => {
+                    return (
+                      <div key={channel.token}>
+                        <span>{channel.name} </span>
+                        <span>
+                          {channel.value.toFixed(0)} {channel.suffix}
+                        </span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+            {gasesChannels.length > 0 && (
+              <>
+                <h3>Gasses:</h3>
+                <div>
+                  {gasesChannels.map((channel) => {
+                    return (
+                      <div key={channel.token}>
+                        <span>{channel.name} </span>
+                        <span>
+                          {channel.value.toFixed(0)} {channel.suffix}
+                        </span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+            {particulatesChannels.length > 0 && (
+              <>
+                <h3>Particulates:</h3>
+                <div>
+                  {particulatesChannels.map((channel) => {
+                    return (
+                      <div key={channel.token}>
+                        <span>{channel.name} </span>
+                        <span>
+                          {channel.value.toFixed(0)} {channel.suffix}
+                        </span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+          </div>
         </div>
       )}
     </div>
