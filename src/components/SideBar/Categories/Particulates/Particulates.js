cbda336f59831bb389f185686d1e1235bcb4dd37
HristoDzhambazki1 <hristo.dzhambazki@barinlabs.com>
2021-08-04 13:27:49 +0300

    added collapse, more styles and functionality

--- src/components/SideBar/Categories/Particulates/Particulates.js
+++ src/components/SideBar/Categories/Particulates/Particulates.js
@@ -2,32 +2,63 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./particulates.module.scss";
 
-import { faBraille } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBraille,
+  faArrowDown,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Particulates = ({ channels }) => {
-  console.log(channels);
+  const [isCollapseOpen, setIsCollapseOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsCollapseOpen(!isCollapseOpen);
+  };
   return (
     <div>
       <h3>Particulates:</h3>
       <div>
         {channels.map((channel) => {
+          const standard = channel.standards[0];
           return (
-            <div key={channel.token} className={styles.channelContainer}>
-              <div className={styles.iconAndStatusContainer}>
-                <div className={styles.statusContainer}>
-                  <div className={styles.iconContainer}>
-                    <FontAwesomeIcon icon={faBraille} size="lg" />
+            <div
+              key={channel.token}
+              className={styles.channelContainer}
+              onClick={toggleCollapse}
+            >
+              <div className={styles.mainContainer}>
+                <div className={styles.iconAndStatusContainer}>
+                  <div className={styles.statusContainer}>
+                    <div className={styles.iconContainer}>
+                      <FontAwesomeIcon icon={faBraille} size="lg" />
+                    </div>
                   </div>
                 </div>
+                <div className={styles.dataContainer}>
+                  <span>{channel.name} </span>
+                  {standard && <span>{standard.percentage * 100}%</span>}
+                  <span>
+                    {channel.value.toFixed(0)} {channel.suffix}
+                  </span>
+                </div>
+                <div className={styles.collapseBtn}>
+                  {isCollapseOpen ? (
+                    <FontAwesomeIcon icon={faArrowUp} size="sm" />
+                  ) : (
+                    <FontAwesomeIcon icon={faArrowDown} size="sm" />
+                  )}
+                </div>
               </div>
-              <div className={styles.dataContainer}>
-                <span>{channel.name} </span>
-                <span>
-                  {channel.value.toFixed(0)} {channel.suffix}
-                </span>
-              </div>
-              <div className={styles.collapseBtn}></div>
-              {/* <div className={styles.descriptionContainer}></div> */}
+              {isCollapseOpen && (
+                <div className={styles.descriptionContainer}>
+                  <h6>European standard quality index</h6>
+                  <p>
+                    loerm
+                    loermlroaromdaimsdiasmdnaindsiasndiansdindasindisandian
+                  </p>
+                </div>
+              )}
             </div>
           );
         })}
